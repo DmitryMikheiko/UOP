f0a7347c327d9ce7c32e090fed70ee18e4710795
unknown <mikheiko.dmitry@gmail.com>
2019-10-04 14:41:07 +0300

    first commit

---
+++ UOP_Test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "lib\UOP.cpp"
+using namespace std;
+int main(){
+    UOP uop;
+    cout<<uop.GetVersion();
+    cin.get();
+    return 0;
+}
---
+++ lib/UOP.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class UOP
+{
+private:
+    const string version = "3.0";
+    /* data */
+public:
+    UOP(/* args */);
+    ~UOP();
+    string GetVersion();
+};
+
+
